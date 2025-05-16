e6976b519f14dcc4448de1749174c841b41365f2
Manuel Constanzo <manuel.constanzo@email.cl>
2025-05-16 02:34:45 -0400

    Style – estilos implementados en proyecto

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-const students = []
+const students=[]
+const tableBody=document.querySelector("#studentsTable tbody");
+const averageDiv=document.getElementById("average");
+document.getElementById("studentForm").addEventListener("submit",function (e){
+ e.preventDefault();
 
-document.getElementById('student-form').addEventListener('submit', function(e){
-    e.preventDefault();
-    const name = document.getElementById('name').value.trim();
-    const lastName = document.getElementById('last-name').value.trim();
-    const grade = document.getElementById('grade').value;
+ const name=document.getElementById("name").value.trim();
+ const lastName=document.getElementById("lastName").value.trim();
+ const grade=parseFloat(document.getElementById("grade").value);
 
-    if(grade < 1 || grade > 7 || isNaN(grade) || !name || !lastName){
-        alert("Error al ingresar los datos")
-        return
-    }
+ if(!name || !lastName || isNaN(grade) || grade<1 || grade>7){
+    alert("Error al ingresar Datos")
+     return
+ }  
+ 
+ const student={name,lastName,grade};
 
-    const student = {name,lastName,grade}
-    students.push(student);
-    console.log(students);
+ students.push(student);
+ //console.log(students)
+addStudentToTable(student)
+calcularPromedio();
+ this.reset()
 
-    addStudentToTable(student);    
-    this.reset();
 });
 
-function addStudentToTable(student) {
-    const tablebody = document.querySelector('#StudentsTable tbody');
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <td>${student.name}</td>
+function addStudentToTable(student){
+    const row=document.createElement("tr");
+    row.innerHTML=`
+       <td>${student.name}</td>
         <td>${student.lastName}</td>
-        <td>${student.grade}</td>
-        <td>${student.date}</td>`;
-    tablebody.appendChild(row);
-}
+        <td>${student.grade}</td>`;
+     tableBody.appendChild(row);   
+       }
+
+       function calcularPromedio(){
+           if(students.length===0){
+             averageDiv.textContent="Promedio General del Curso : N/A"
+             return
+           }
+            const total=students.reduce((sum,student)=>sum+student.grade,0);
+           const prom=total/students.length;
+           averageDiv.textContent="Promedio General del Curso : "+prom.toFixed(2);
+       }
+
